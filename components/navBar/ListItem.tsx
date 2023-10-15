563fb2bd7cf676122e0e28332a004d2d6d4caf88
Mostofa321 <hmshumon123@gmail.com>
2023-10-15 17:43:50 +0600

    add- added navbar section

---
+++ components/navBar/ListItem.tsx
@@ -0,0 +1,55 @@
+import { FC } from 'react'
+import {
+    Typography,
+} from "../materialTailwind/MaterialComponents";
+import Link from 'next/link';
+
+interface LiTypes {
+    LiElement?: string,
+    route?: string,
+}
+
+const ListItem: FC<LiTypes> = ({ LiElement, route }) => {
+    return (
+        <Typography
+            as="li"
+            variant="small"
+            color="blue-gray"
+            className="p-1 font-medium"
+        >
+            <Link href={`${route}`}
+                className="
+                    text-base 
+                    font-semibold 
+                    hover:text-[#e66030] 
+                    transition-colors
+                    py-5
+                    px-2 
+                    relative 
+                    after:content-[''] 
+                    after:absolute 
+                    after:w-full 
+                    after:h-[3px] 
+                    after:bottom-[8px] 
+                    after:left-0 
+                    after:bg-[#e66030] 
+                    after:invisible 
+                    after:scale-x-0 
+                    after:transition-all 
+                    after:duration-150 
+                    after:ease-in 
+                    after:z-[20] 
+                    hover:after:visible 
+                    hover:after:scale-x-100 
+                    hover:after:transition-all 
+                    hover:after:duration-300 
+                    hover:after:ease-out
+            ">
+                {LiElement}
+            </Link>
+        </Typography>
+    )
+}
+
+export default ListItem
+// px-2 relative after:content-[''] after:absolute after:w-full after:h-[3px] after:-bottom-[12px] after:left-0 after:bg-[#e66030] after:invisible after:scale-x-0 after:transition-all after:duration-150 after:ease-in after:z-[20] hover:after:visible hover:after:scale-x-100 hover:after:transition-all hover:after:duration-300 hover:after:ease-out
---
+++ components/navBar/Nav.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import {
+    Navbar,
+    Collapse,
+    IconButton,
+} from "../materialTailwind/MaterialComponents";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import NavList from './NavList';
+
+
+
+export const Nav = () => {
+    const [openNav, setOpenNav] = useState(false);
+
+    const handleWindowResize = () =>
+        window.innerWidth >= 960 && setOpenNav(false);
+
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+
+    return (
+        <div className="shadow-md sticky top-0 z-20 bg-[white] p-3">
+            <Navbar className="mx-auto shadow-none max-w-screen-2xl px-4 py-2">
+                <div className="flex items-center justify-between text-blue-gray-900">
+                    <Image src="/logo.png" width={250} height={250} alt={"Logo"} />
+                    <div className="hidden lg:block">
+                        <NavList />
+                    </div>
+                    <IconButton
+                        variant="text"
+                        className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                        ripple={false}
+                        onClick={() => setOpenNav(!openNav)}
+                    >
+                        {openNav ? (
+                            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+                        ) : (
+                            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+                        )}
+                    </IconButton>
+                </div>
+                <Collapse open={openNav}>
+                    <NavList />
+                </Collapse>
+            </Navbar>
+        </div>
+    );
+}
